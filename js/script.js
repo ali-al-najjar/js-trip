6feecaa0d6923d5855e207fb6b2e32a8cce37b66
aliynajjar <ali.y.alnajjar@gmail.com>
2023-02-26 18:25:24 +0200

    Added validation method

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.onload=function(){
+let form = document.getElementsByName("form");
 let first_name = document.getElementById("first_name");
 let last_name = document.getElementById("last_name");
 let email = document.getElementById("email");
@@ -6,8 +7,14 @@ let password = document.getElementById("password");
 let profession = document.getElementById("profession");
 let submit = document.getElementById("submit");
 let form_title = document.getElementById("form_title");
-let profile = [];
 
+// function displayError(label) {
+//   let p = document.createElement("p");
+//   p.innerhtml= `Please pro`
+
+// }
+
+// displayError();
 submit.addEventListener("click", function(){
     let profile_data = {
         "first_name":first_name.value,
@@ -20,13 +27,29 @@ submit.addEventListener("click", function(){
 })
 
 function validateForm() {
-  let name = document.forms["form"]["first_name"].value;
-  if( name == ""){
-     alert( "Please provide your First Name!" );
-     name.focus();
+  let fname = document.forms["form"]["first_name"].value;
+  let lname = document.forms["form"]["last_name"].value;
+  let email = document.forms["form"]["email"].value;
+  let password = document.forms["form"]["password"].value;
+  let profession = document.forms["form"]["profession"].value;
+
+  if(fname == ""){
+     alert("First name is required!");
      return false;
-  }
-return true;
+  }else if(lname == ""){
+    alert("Last name is required!");
+    return false;
+ }else if(email == ""){
+  alert("Email is required!");
+  return false;
+}else if(password == ""){
+  alert("Password is required!");
+  return false;
+}else if(profession == ""){
+  alert("Profession is required!");
+  return false;
+}else
+  return true;
 }
 
 submit.addEventListener("click",validateForm);
